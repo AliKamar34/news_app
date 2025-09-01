eaf7247c592e7867d9aaf88d6c815edf433635af
Ali kamar <aliekamar123@gmail.com>
2025-09-02 02:00:11 +0300

    update build.gradle.kts with specific NDK version; add Arabic and English translations; refactor main.dart and news_app.dart for localization support

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const whiteColor = Colors.white;
   static const blackColor = Colors.black;
-  static const borderColor = Color(0xffE8ECF4);
+  static const secondaryColor = Color(0xffF0EFF0);
 }
--- lib/news_app.dart
+++ lib/news_app.dart
@@ -1,10 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/core/routing/router_generation_config.dart';
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SizedBox());
+    return MaterialApp.router(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      routerConfig: RouterGenerationConfig.goRoute,
+    );
   }
 }
