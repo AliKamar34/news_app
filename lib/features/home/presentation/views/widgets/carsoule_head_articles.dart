bc34d0dc8f8c38465a93445796188e61d762a9d9
Ali kamar <aliekamar123@gmail.com>
2025-09-12 02:27:35 +0300

    add article list to implement data in ui

--- lib/features/home/presentation/views/widgets/carsoule_head_articles.dart
+++ lib/features/home/presentation/views/widgets/carsoule_head_articles.dart
@@ -1,23 +1,24 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/features/home/data/models/article_model.dart';
 import 'package:news_app/features/home/presentation/views/widgets/head_article_item.dart';
 
 class CasouleHeadArticle extends StatelessWidget {
-  const CasouleHeadArticle({super.key});
-
+  const CasouleHeadArticle({super.key, required this.article});
+  final List<ArticleModel> article;
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
       options: CarouselOptions(
-        height: MediaQuery.of(context).size.height * 0.35,
+        height: MediaQuery.of(context).size.height * 0.36,
         autoPlay: true,
         autoPlayInterval: const Duration(seconds: 3),
         enlargeCenterPage: true,
       ),
-      items: [1, 2, 3, 4, 5].map((i) {
+      items: article.map((i) {
         return Builder(
           builder: (BuildContext context) {
-            return const HeadArticleItem();
+            return HeadArticleItem(article: i);
           },
         );
       }).toList(),
--- lib/features/home/presentation/views/widgets/custom_articles_list_vew.dart
+++ lib/features/home/presentation/views/widgets/custom_articles_list_vew.dart
@@ -1,14 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/core/themes/app_text_styles.dart';
+import 'package:news_app/core/utils/format_date_string.dart';
+import 'package:news_app/features/home/data/models/article_model.dart';
 
 class CustomArticlesListView extends StatelessWidget {
-  const CustomArticlesListView({super.key});
-
+  const CustomArticlesListView({super.key, required this.articles});
+  final List<ArticleModel> articles;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 5,
+      itemCount: articles.length,
       itemBuilder: (context, index) {
         return ListTile(
           contentPadding: const EdgeInsets.symmetric(
@@ -16,13 +18,27 @@ class CustomArticlesListView extends StatelessWidget {
             vertical: 8,
           ),
           title: Text(
-            'head article title ',
+            articles[index].title ?? '',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
             style: AppTextStyle.styleSemiBold18(context),
           ),
           subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Author name', style: AppTextStyle.styleRegular12(context)),
-              Text('May 1, 2025', style: AppTextStyle.styleRegular12(context)),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.20,
+                child: Text(
+                  articles[index].authorName ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyle.styleRegular12(context),
+                ),
+              ),
+              Text(
+                formatDateString(articles[index].publishedAt),
+                style: AppTextStyle.styleRegular12(context),
+              ),
             ],
           ),
           trailing: SizedBox(
@@ -31,9 +47,16 @@ class CustomArticlesListView extends StatelessWidget {
               borderRadius: BorderRadius.circular(12),
               child: CachedNetworkImage(
                 imageUrl:
+                    articles[index].imageUrl ??
                     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpuz18BdRbDxEi0_YA3Q9zXq90dTKHKM0WDUZQKZe0DKLQ8jlxZqROkXE&s',
                 width: double.infinity,
                 fit: BoxFit.fill,
+                errorWidget: (context, url, error) => Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpuz18BdRbDxEi0_YA3Q9zXq90dTKHKM0WDUZQKZe0DKLQ8jlxZqROkXE&s',
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
           ),
