0bba3ad114416499e93577a7a4996735d2b4f151
Ali kamar <aliekamar123@gmail.com>
2025-09-12 02:28:11 +0300

    create custom article section

---
+++ lib/features/home/presentation/views/widgets/custom_article_section.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/features/home/presentation/manager/cubit/get_articles_cubit.dart';
+import 'package:news_app/features/home/presentation/views/widgets/carsoule_head_articles.dart';
+import 'package:news_app/features/home/presentation/views/widgets/custom_articles_list_vew.dart';
+import 'package:news_app/features/home/presentation/views/widgets/custom_home_loading_widget.dart';
+
+class CustomArticlesSection extends StatelessWidget {
+  const CustomArticlesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetArticlesCubit, GetArticlesState>(
+      builder: (context, state) {
+        if (state is GetArticlesSuccess) {
+          return Column(
+            children: [
+              CasouleHeadArticle(article: state.articles.take(6).toList()),
+              Expanded(
+                child: CustomArticlesListView(
+                  articles: state.articles.skip(6).toList(),
+                ),
+              ),
+            ],
+          );
+        } else if (state is GetArticlesFailure) {
+          return Center(child: Text(state.errorMessage));
+        } else {
+          return const CustomHomeLoadingWidget();
+        }
+      },
+    );
+  }
+}
