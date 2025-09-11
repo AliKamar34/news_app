9898d926b87d30dd84c1a8c4121eed01071c2dbd
Ali kamar <aliekamar123@gmail.com>
2025-09-12 02:15:45 +0300

    init secure storage , save data  and implement bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/helpers/secure_storage_helper.dart';
+import 'package:news_app/core/secrets.dart';
+import 'package:news_app/core/utils/simple_bloc_observer.dart';
 import 'package:news_app/news_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
+  await SecureStorageHelper.saveDate('ApiKey', Secrets.apiKey);
+  Bloc.observer = SimpleBlocObserver();
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
