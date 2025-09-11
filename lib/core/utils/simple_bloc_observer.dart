e4938a1422dd4f13340b9dab4d8055d077562a70
Ali kamar <aliekamar123@gmail.com>
2025-09-12 01:51:56 +0300

    add SimpleBlocObserver class for managing Bloc state changes and logging

---
+++ lib/core/utils/simple_bloc_observer.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver implements BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    // super.onChange(bloc, change);
+    debugPrint('$change');
+    log(change.toString());
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    // super.onTransition(bloc, transition);
+    debugPrint('$transition');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    debugPrint('closed $bloc');
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    debugPrint('create $bloc');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {}
+}
